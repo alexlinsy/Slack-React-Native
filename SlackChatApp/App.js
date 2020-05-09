0522370b85f5fc32f6f7746e05f993cfbd5131b5
Shaoyi Lin <shaoyilin@Shaoyis-Mac-mini.local>
2020-05-09 18:09:05 -0300

    Setup App.js for basic view

--- SlackChatApp/App.js
+++ SlackChatApp/App.js
@@ -1,114 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import {NavigationContainer} from '@react-navigation/native';
+
+const ChannelScreen = ({navigation, route}) => (
+  <SafeAreaView>
+      <Text>Channel Screen</Text>
+    </SafeAreaView>
+);
+
+const ChannelListDrawer = (props) => (
+  <SafeAreaView>
+    <Text>Drawer</Text>
+  </SafeAreaView>
+);
+
+const Drawer = createDrawerNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <View style={styles.container}>
+      <Drawer.Navigator
+        drawerContent={ChannelListDrawer}
+        drawerStyle={styles.drawerNavigator}
+      >
+        <Drawer.Screen name="ChannelScreen" component={ChannelScreen} />
+      </Drawer.Navigator>
+    </View>
+  </NavigationContainer>
+)
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  channelScreenSaveAreaView: {
+    backgroundColor: 'white'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  channelScreenContainer: {
+    flexDirection:'column',
+    height: '100%'
   },
-  body: {
-    backgroundColor: Colors.white,
+  container: {
+    flex: 1,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  drawerNavigator: {
+    backgroundColor: '#3F0E40',
+    width: 350,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  chatContainer: {
+    backgroundColor: 'white',
+    flexGrow: 1,
+    flexShrink: 1
+  }
 });
 
 export default App;
