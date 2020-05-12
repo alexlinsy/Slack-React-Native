d2b477aedaff5142b18000e4b4c945bfb9fd59c8
Shaoyi Lin <shaoyilin@Shaoyis-Mac-mini.local>
2020-05-12 16:39:28 -0300

    Implemented list items into channel list

---
+++ SlackChatApp/src/customHooks/useWatchedChannels.js
@@ -0,0 +1,73 @@
+import {useState, useEffect} from 'react';
+
+export const useWatchedChannels = (client, changeChannel) => {
+    const [activeChannelId, setActiveChannelId] = useState(null);
+    const [unreadChannels, setUnreadChannels] = useState([]);
+    const [readChannels, setReadChannels] = useState([]);
+    const [oneOnOneConversations, setOneOnOneConversations] = useState(true);
+    const [hasMoreChannels, setHasMoreChannels] = useState(true);
+    
+    const filters={
+      type: 'messaging',
+      example: 'slack-demo',
+      members: {
+        $in: [client.user.id]
+      }
+    };
+  
+    const sort = {has_unread: -1, cid: -1};
+    const options = {limit: 30, state: true};
+  
+    useEffect(() => {
+      if(!hasMoreChannels) {
+        return;
+      }
+  
+      let offset = 0;
+      const _unreadChannels = [];
+      const _readChannels = [];
+      const _oneOnOneConversations = [];
+  
+      async function fetchChannels() {
+        const channels = await client.queryChannels(filters, sort, {
+          ...options,
+          offset,
+        });
+  
+        offset = offset + channels.length;
+        channels.forEacch(c => {
+          if(c.countUnread() > 0) {
+            _unreadChannels.push(c);
+          } else if(Object.keys(c.state.members).length === 2) {
+            _oneOnOneConversations.push(c);
+          } else {
+            _readChannels.push(c);
+          }
+        });
+  
+        setUnreadChannels([..._unreadChannels]);
+        setReadChannels([..._readChannels]);
+        setOneOnOneConversations([...oneOnOneConversations]);
+  
+        if(channels.length === options.limit) {
+          fetchChannels();
+        } else {
+          setHasMoreChannels(false);
+          setActiveChannelId(_readChannels[0].id);
+          changeChannel(_readChannels[0].id);
+        }
+      }
+      fetchChannels();
+    }, [client]);
+  
+    return {
+      activeChannelId,
+      setActiveChannelId,
+      unreadChannels,
+      setUnreadChannels,
+      readChannels,
+      setReadChannels,
+      oneOnOneConversations,
+      setOneOnOneConversations,
+    };
+  };
