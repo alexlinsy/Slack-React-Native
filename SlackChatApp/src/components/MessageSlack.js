654cedd66654ad4bd292611a836544c05c335da4
Shaoyi Lin <shaoyilin@Shaoyis-Mac-mini.local>
2020-06-01 02:04:58 -0300

    Modify functions

--- SlackChatApp/src/components/MessageSlack.js
+++ SlackChatApp/src/components/MessageSlack.js
@@ -1,56 +1,27 @@
 import React from 'react';
-import {
-  MessageSimple
-} from 'stream-chat-react-native';
-import {
-  MessageFooter
-} from './MessageFooter';
-import {
-  MessageText
-} from './MessageText';
-import {
-  MessageAvatar
-} from './MessageAvatar';
-import {
-  MessageHeader
-} from './MessageHeader';
-import {
-  UrlPreview
-} from './UrlPreview';
-import {
-  Giphy
-} from './Giphy';
+import {MessageSimple} from 'stream-chat-react-native';
+import {MessageFooter} from './MessageFooter';
+import {MessageText} from './MessageText';
+import {MessageAvatar} from './MessageAvatar';
+import {MessageHeader} from './MessageHeader';
+import {UrlPreview} from './UrlPreview';
+import {Giphy} from './Giphy';
 
 export function MessageSlack(props) {
   if (props.message.deleted_at) {
     return null;
   }
-  return ( <
-    MessageSimple {
-      ...props
-    }
-    forceAlign = "left"
-    ReactionList = {
-      null
-    }
-    MessageAvatar = {
-      MessageAvatar
-    }
-    MessageHeader = {
-      MessageHeader
-    }
-    MessageFooter = {
-      MessageFooter
-    }
-    MessageText = {
-      MessageText
-    }
-    UrlPreview = {
-      UrlPreview
-    }
-    Giphy = {
-      Giphy
-    }
+  return (
+    <MessageSimple
+      {...props}
+      forceAlign="left"
+      ReactionList={null}
+      MessageAvatar={MessageAvatar}
+      MessageHeader={MessageHeader}
+      MessageFooter={MessageFooter}
+      MessageText={MessageText}
+      UrlPreview={UrlPreview}
+      Giphy={Giphy}
     />
   );
-};
+}
